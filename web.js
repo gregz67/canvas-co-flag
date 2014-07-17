5eec87cf7ae8301734f8eb35b36e29526a5ff39c
Greg Z <ziebold@gmail.com>
2014-07-17 11:14:15 -0600

    simplify node web server

--- web.js
+++ web.js
@@ -3,13 +3,9 @@ var morgan = require("morgan");
 
 var app = express();
 app.use(morgan());
-
-app.configure(function() {
-    app.use(express.static(__dirname));
-});
+app.use(express.static(__dirname));
 
 var port = process.env.PORT || 8080;
-
 app.listen(port, function() {
       console.log("Listening on " + port);
 });
