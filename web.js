7c9068d34b3cdb67934e6163430614b1bd8abccb
Greg Z <ziebold@gmail.com>
2014-07-23 15:45:56 -0600

    add env port for Cloud Foundry

--- web.js
+++ web.js
@@ -5,7 +5,7 @@ var app = express();
 app.use(morgan());
 app.use(express.static(__dirname));
 
-var port = process.env.PORT || 8080;
+var port = process.env.PORT ||process.env.VCAP_APP_PORT || 8080;
 app.listen(port, function() {
       console.log("Listening on " + port);
 });
